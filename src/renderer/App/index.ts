1c81b06d0a6c7650a4a2f72daa3cd4406f775fea
bluefrog <bluefrog.wu@gmail.com>
2023-05-23 09:40:45 +0800

    清除之前的测试应用

--- src/renderer/App/index.ts
+++ src/renderer/App/index.ts
@@ -1,15 +1,5 @@
 // 定义 App
 const appList = [
-  // {
-  //   key: 'Demo1',
-  //   icon: "",
-  //   label: 'Demo1',
-  // },
-  // {
-  //   key: 'Demo2',
-  //   icon: "",
-  //   label: 'Demo2',
-  // },
   {
     key: 'Hash',
     icon: "",
