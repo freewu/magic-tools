1f775a73c3b6844f4217ea297fc55fdfc0f03362
bluefrog <bluefrog.wu@gmail.com>
2023-06-30 08:33:47 +0800

    bugfix 修复 RMBConvert 交互处理上的 bug

--- src/renderer/App/RMBConvert/index.tsx
+++ src/renderer/App/RMBConvert/index.tsx
@@ -3,11 +3,13 @@ import { useState } from "react";
 const { TextArea } = Input;
 import { copyTextToClipboard } from "./../../lib"
 import { convertCurrency } from "./lib"
+import { InputStatus } from "antd/es/_util/statusUtils";
 
 const RMBConvert = () => {
 
-  const [ encodeValue, setEncodeValue ] = useState('');
-  const [ decodeValue, setDecodeValue ] = useState('');
+  const [ status, setStatus ] = useState('');
+  const [ value, setValue ] = useState('');
+  const [ result, setResult ] = useState('');
   const [ notice, contextHolder ] = message.useMessage(); // 消息提醒
 
   const textareaDoubleClick = (e :React.MouseEvent<HTMLTextAreaElement>) => {
@@ -20,14 +22,21 @@ const RMBConvert = () => {
 
   const encode = (value :string) => {
     let v = value.trim()
+    setValue(v);
     if ( v != "") {
-      setEncodeValue(v);
       // 去掉空格 去掉 `,` (有使用 ,做分隔展示的情况 )
       v = v.replace(/,/g,'').replace(/\s/g,'');
       // 判断是否是 全数字
-      if(/^[0-9\.].*$/.test(v)) {
-        setDecodeValue(convertCurrency(v));
+      if(/^[0-9\.]+$/.test(v)) {
+        setResult(convertCurrency(v));
+        setStatus('');
+      } else {
+        setStatus('error');
+        setResult('');
       }
+    } else {
+      setStatus('');
+      setResult('');
     }
   }
 
@@ -37,19 +46,20 @@ const RMBConvert = () => {
       {contextHolder}
 
       <Button 
-        onClick={ () => { setEncodeValue(''); setDecodeValue(''); } }
+        onClick={ () => { setValue(''); setResult(''); setStatus(''); } }
         style={ {"backgroundColor" : "#dc3545","color": "#fff" }} 
       >清除</Button>
 
       <TextArea
+        status= { status as InputStatus }
         style={ { margin: "5px 0 5px 0" }}
         onDoubleClick={ textareaDoubleClick }
         onChange={ (e) => { encode(e.target.value) ;} }
         title="双击复制内容到粘贴板"
-        value= { encodeValue }
+        value= { value }
         placeholder="请输入要转换的人民币金额"
         autoSize={{ minRows: 5, maxRows: 5 }}
-        maxLength={ 10 }
+        maxLength={ 30 }
       />
 
       <Divider>转换的结果</Divider>
@@ -59,7 +69,7 @@ const RMBConvert = () => {
         style={ { margin: "5px 0 5px 0" }}
         title="点击复制内容到粘贴板"
         onClick={ textareaDoubleClick }
-        value= { decodeValue }
+        value= { result }
         placeholder="点击复制内容到粘贴板"
         autoSize={{ minRows: 5, maxRows: 5 }}
       />
