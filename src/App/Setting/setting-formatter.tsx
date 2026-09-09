1558754f266f33531a27bd7d5ed59ff71f7cbd4e
bluefrog <bluefrog.wu@gmail.com>
2026-09-09 19:59:42 +0800

    fix(ui): 设置-格式化分组移除"暂无独立设置项"提示文案

--- src/App/Setting/setting-formatter.tsx
+++ src/App/Setting/setting-formatter.tsx
@@ -1,7 +1,5 @@
-import { Form, Typography } from "antd";
+import { Form } from "antd";
 import HtmlFormatSetting from "../HtmlFormat/setting";
-
-const { Text } = Typography;
 
 // 格式化分类: JSON 格式化 / JSON5 格式化 / SQL 格式化 / HTML 格式化 / SVG 格式化 / 中英文自动排版
 // JSON/JSON5/SQL/SVG/CnEn 暂无独立配置项; HTML 格式化提供默认缩进设置
@@ -9,7 +7,6 @@ export const SettingFormatter = () => {
 
   return (
     <Form labelCol={{ span: 5 }} wrapperCol={{ span: 18 }} layout="horizontal" style={{ maxWidth: 800 }}>
-      <Text type="secondary">JSON 格式化 / JSON5 格式化 / SQL 格式化 / SVG 格式化 / 中英文自动排版 暂无独立设置项</Text>
       <HtmlFormatSetting />
     </Form>
   )
